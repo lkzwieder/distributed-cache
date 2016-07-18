8b82cf4e73c3dbad83f0f21e7b30708792e34d6f
Lucas Tettamanti <lucas.tettamanti@gmail.com>
2016-07-18 15:38:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var redis = require("redis");
+module.exports = function(options, callback) {
+   options = options || {host: "127.0.0.1", port: 6379};
+   var defaultPool = "commons";
+   var client = redis.createClient(options, callback);
+
+   function close() {
+      client.quit();
+   }
+
+   function hset(key, value, pool, callback) {
+      pool = pool || defaultPool;
+      client.hset(pool, key, value, callback);
+   }
+
+   function hmset(arr, pool, callback) {
+      pool = pool || defaultPool;
+      var obj = {};
+      arr.forEach(function(a) {
+         for(var k in a) {
+            obj[k] = a[k];
+         }
+      });
+      client.hmset(pool, obj, callback);
+   }
+
+   function hget(key, callback, pool) {
+      pool = pool || defaultPool;
+      client.hget(pool, key, callback);
+   }
+
+   function hmget(arr, callback, pool) {
+      pool = pool || defaultPool;
+      client.hmget(pool, arr, callback);
+   }
+
+   function hgetAll(pool, callback) {
+      client.hgetall(pool, callback);
+   }
+
+   return {
+      close: close,
+      get: hget,
+      multiGet: hmget,
+      set: hset,
+      multiSet: hmset,
+      all: hgetAll
+   };
+};
